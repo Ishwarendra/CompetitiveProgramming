449cabc6c47a2ae267aa0f9ff174de140882c6ee
Ishwarendra Jha <75680424+Ishwarendra@users.noreply.github.com>
2022-09-25 16:22:36 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1260C | Infinite Fence/162580444.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+ 
+#ifdef LOCAL
+#include <F:\CPP\Debug\debug.h> 
+#else
+#define trace(...) 1;
+#define print(...) 1;
+#endif
+ 
+using i64 = long long;
+ 
+void solve()
+{
+    i64 r, b, k;
+    std::cin >> r >> b >> k;
+ 
+    i64 g = std::gcd(r, b);
+    r /= g, b /= g;
+ 
+    if (r > b)  
+        std::swap(r, b);
+ 
+    if ((b - 1 + k - 2) / (k - 1) > r)
+        std::cout << "REBEL\n";
+    else
+        std::cout << "OBEY\n";
+}
+ 
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+ 
+    int t = 1;
+    std::cin >> t;
+    
+    while (t--)
+        solve();
+    
+    return 0;
+}
