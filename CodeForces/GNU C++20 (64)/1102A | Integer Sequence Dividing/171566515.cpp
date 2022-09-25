9eb55d14591e7503dfe77819d3cc42c74a18caef
Ishwarendra Jha <75680424+Ishwarendra@users.noreply.github.com>
2022-09-25 16:06:59 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1102A | Integer Sequence Dividing/171566515.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+ 
+#ifdef LOCAL
+#include "F:\CPP\Debug\debug.h" 
+#else
+#define print(...) 1;
+#endif
+ 
+using i64 = long long;
+ 
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    int n;
+    std::cin >> n;
+ 
+    i64 sum = 1LL * n * (n + 1) / 2;
+    if (sum % 2)
+        std::cout << "1\n";
+    else
+        std::cout << "0\n";
+    
+    return 0;
+}
