2fae716f7628131e559e39c16994201e02733400
Ishwarendra Jha <75680424+Ishwarendra@users.noreply.github.com>
2022-09-25 16:31:38 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1632C | Strange Test/157938306.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+ 
+#ifdef LOCAL
+ #include <F:\CPP\Debug\debug.h> 
+#else
+ #define trace(x...) 1;
+ #define print(x...) 1;
+#endif
+ 
+void solve()
+{
+ int a, b;
+ std::cin >> a >> b;
+ 
+ int ans = b - a;
+ 
+ int new_a = a;
+ 
+ while (new_a <= b)
+ {
+  int x = new_a - a;
+  
+  for (int y = 0; y < b - a; y++)
+   ans = std::min(ans, x + y + 1 + ((a + x) | (b + y)) - (b + y));
+  
+  new_a = (new_a | (new_a - 1)) + 1;
+ }
+ 
+ std::cout << ans << '\n';
+}
+ 
+int main()
+{
+ std::ios::sync_with_stdio(false);
+ std::cin.tie(nullptr);
+ 
+ int t = 1;
+ std::cin >> t;
+ 
+ while(t--)
+  solve();
+ 
+ return 0;
+}
